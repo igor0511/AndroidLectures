78a6fa328df973241b82189824fffd46519c3ed8
Marko Maric <calemaric@gmail.com>
2016-10-30 20:01:05 +0100

    Final sa komentarima

--- app/src/main/java/rs/fon/kalkulator/MainActivity.java
+++ app/src/main/java/rs/fon/kalkulator/MainActivity.java
@@ -13,6 +13,28 @@ import android.widget.Toast;
 import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
+    /*
+    * static final polje predstavlja konstantu u Java programskom jeziku.
+    *
+    * Prve dve konstante oznacavaju dve operacije koje cemo koristiti, sabiranje i oduzimanje.
+    * Ove dve operacije su oznacene kao konstante iz razloga sto se u daljem kodu mozemo pozivati na
+    * njih, a ne pamtiti vrednost. Tako mozemo izbeci eventualne greske u kucanju operacija.
+    *
+    * Druge dve konstante oznacavaju stringove koji sluze za pamcenje stanja nase aplikacije. Naime,
+    * ukoliko, iz bilo kog razloga, nasa aktivnost bude unistena, tako da mora ponovo da se pozove onCreate
+    * metoda, sva polja unutar ove klase ce biti ponovo inicijalizovana, sto ce dovesti do gazenja stanja
+    * ove aktivnosti (stanje je prethodni rezultat, broj koji smo kucali i istorija operacija). Da bismo
+    * sprecili gubljenje stanja, koristimo onSaveInstanceState metodu, u kojoj Bundle objektu dodeljujemo
+    * promenljive stanja sa imenima ovih konstanti.
+    *
+    * Nakon toga sledi deklaracija svih polja koje cemo koristiti u nasoj aktivnosti.
+    *   - operation je trenutna operacija
+    *   - currentNumber je trenutni broj kao String
+    *   - resultNumber je rezultat operacije
+    *   - numberDisplay je string koji predstavlja istoriju operacija koja pise u bottomDisplay
+    *
+    * bottomDisplay i topDisplay su TextView-ovi iz activity_main layout fajla.
+    * */
     private static final String OPERATION_ADD = "addition";
     private static final String OPERATION_SUB = "subtract";
     private static final String STATE_RESULT = "RESULT";
@@ -26,6 +48,14 @@ public class MainActivity extends AppCompatActivity {
 
     private TextView bottomDisplay = null;
     private TextView topDisplay = null;
+
+    /*
+    *
+    * onCreate je prvi i jedini obavezni callback zivotnog ciklusa aktivnosti. U njemu se vrsi deklaracija
+    * layout i toolbar, kao i inicijalizacija svih polja ove klase. Takodje, unutar njega, mi vadimo,
+    * ukoliko postoje, sve vrednosti iz prethodnog stanja. To vrsimo preko parametra onCreate metode savedInstanceState.
+    *
+    * */
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,18 +81,27 @@ public class MainActivity extends AppCompatActivity {
         bottomDisplay = (TextView) findViewById(R.id.bottom_display);
         topDisplay = (TextView) findViewById(R.id.top_display);
 
-        bottomDisplay.setText(numberDisplay);
-        topDisplay.setText(String.valueOf(resultNumber));
+        bottomDisplay.setText("");
+        topDisplay.setText("");
     }
 
     @Override
     public void onSaveInstanceState(Bundle savedInstanceState) {
-        savedInstanceState.putInt(STATE_RESULT, resultNumber);
-        savedInstanceState.putString(STATE_CURRENT, currentNumber);
-        savedInstanceState.putString(STATE_DISPLAY, numberDisplay);
+        savedInstanceState.putString(STATE_CURRENT,currentNumber);
+        savedInstanceState.putInt(STATE_RESULT,resultNumber);
+        savedInstanceState.putString(STATE_DISPLAY,numberDisplay);
 
         super.onSaveInstanceState(savedInstanceState);
     }
+
+
+    /*
+    * Nakon kreiranja toolbara, poziva se ova metoda, u kojoj se poziva inflate metoda sa menijem koji
+    * se nalazi u res/drawable/menu.
+    *
+    * Ovaj postupak postavlja sve ono sto se nalazi u tom fajlu na glavni toolbar
+    *
+    * */
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -70,17 +109,39 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
+    /*
+    *
+    * Ova metoda definise sta se desava kada korisnik klikne na neku opciju unutar menija.
+    * Kao sto vidite, preko id-ja item elementa iz meni fajla mozemo da odredimo koju je tacno
+    * opciju korisnik odabrao.
+    *
+    * */
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
 
         if (id == R.id.advanced_calculator) {
+            /*
+            * makeTest staticka metoda iz Toast klase pravi takozvanu Toast poruku, tekstom
+            * "Not yet implemeted", pogledati sta je Toast poruka pokretanjem aplikacije i klikom na
+            * opciju iz menija.
+            * */
+
             Toast.makeText(this,"Not yet implemented",Toast.LENGTH_SHORT).show();
             return true;
         }
 
         return super.onOptionsItemSelected(item);
     }
+
+    /*
+    * onAddClicked i onSubtractClicked su metode koje se pozivaju kada korisnik pritisne dugme za
+    * sabiranje ili dugme za oduzimanje. Metode su jako slicne.
+    *
+    * Prvo postavljamo operaciju na operaciju koja je izabrana, zatim pozivamo convertToNumber() metodu,
+    * koja pretvara currentNumber iz string-a u int. Zatim se izvrsava executeOperation metoda, koja
+    * izvrsava operaciju. Zatim se reinicijalizuje trenutni broj i prikazuje se operacija na ekranu.
+    * */
 
     public void onAddClicked(View view) {
         if (operation.isEmpty()) {
@@ -112,8 +173,14 @@ public class MainActivity extends AppCompatActivity {
         displayNumbers("-");
     }
 
-    public void onNumberClicked(View view) {
+    /*
+    * Ovde se iz Button-a koji predstavlja dugme na kome se nalazi broj vadi vrednost njegovog stringa,
+    * tj. broj koji se nalazi na tom dugmetu, zatim se trenutnom broj pridruzuje ta cifra, nakon cega
+    * se ta cifra prikazuje na ekranu.
+    *
+    * */
 
+    public void onNumberClicked(View view) {
         if (view instanceof Button) {
             Button button = (Button) view;
             String numberAsString = button.getText().toString();
@@ -122,6 +189,10 @@ public class MainActivity extends AppCompatActivity {
             displayNumbers(numberAsString);
         }
     }
+
+    /*
+    * Event za dugme koje vraca sve na prvobitno stanje.
+    * */
 
     public void onCEClicked(View view) {
         resultNumber = 0;
@@ -132,6 +203,12 @@ public class MainActivity extends AppCompatActivity {
         bottomDisplay.setText("");
         topDisplay.setText("");
     }
+
+    /*
+    * Event za dugme jednako. Izvrsava se operacija slicno kao u eventovima za operacije, samo sto se
+    * nakon toga sa gornjeg TextView rezultat prebacuje na donji i reinicijalizuju se sva ostala polja.
+    *
+    * */
 
     public void onEqualsClicked(View view) {
         int res = convertToNumber();
@@ -148,6 +225,9 @@ public class MainActivity extends AppCompatActivity {
         resultNumber = 0;
     }
 
+    /*
+    * Metoda za prikaz na ekranu.
+    * */
     private void displayNumbers(String character) {
         String[] numbers = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "0"};
         String[] operations = {"-","+"};
@@ -159,6 +239,11 @@ public class MainActivity extends AppCompatActivity {
                 return;
             }
         }
+
+        /*
+        * Ovde je ukljucen slucaj kada korisnik ukuca dve operacije za redom. Metoda brise prethodnu
+        * i stavlja novu operaciju.
+        * */
 
         if (numberDisplay.length() > 0) {
             if (Arrays.asList(operations).contains(numberDisplay.substring(numberDisplay.length() -1))) {
@@ -181,6 +266,12 @@ public class MainActivity extends AppCompatActivity {
             return 0;
         }
     }
+
+    /*
+    * Metoda koja izvrsava operaciju, tako sto proverava koja je operacija u pitanju, a zatim
+    * je izvrsi sa parametrom number i pridruzuje rezultat resultNumber polju. Rezultat upisuje u
+    * gornji TextView.
+    * */
 
     private void executeOperation(String operation, int number) {
         switch (operation) {
